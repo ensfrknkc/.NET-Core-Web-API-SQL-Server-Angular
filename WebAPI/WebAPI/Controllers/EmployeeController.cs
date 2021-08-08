dc43717b92a86cc929e99dc338230dbb94a78dd7
ensfrknkc <ensfrknkc58@gmail.com>
2021-08-08 20:42:07 +0300

    SaveFile Added

--- WebAPI/WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/WebAPI/Controllers/EmployeeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using WebAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace WebAPI.Controllers
 {
@@ -16,9 +18,11 @@ namespace WebAPI.Controllers
     public class EmployeeController : ControllerBase
     {
          private readonly IConfiguration _configuration;
-        public EmployeeController(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment webHostEnvironment )
         {
             _configuration = configuration;
+            _env = webHostEnvironment;
         }
 
         [HttpGet]
@@ -131,5 +135,30 @@ namespace WebAPI.Controllers
             }
             return new JsonResult("Delete Succesfuly");
         }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using(var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("anonymous.png");
+            }
+        }
+
     }
 }
