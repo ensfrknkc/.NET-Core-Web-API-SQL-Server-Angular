36f341877ba73c9c216f2d2164aac26c5137466c
ensfrknkc <ensfrknkc58@gmail.com>
2021-08-08 18:16:51 +0300

    EmployeeController Updated

--- WebAPI/WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/WebAPI/Controllers/EmployeeController.cs
@@ -4,6 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -11,5 +15,121 @@ namespace WebAPI.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+         private readonly IConfiguration _configuration;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select EmployeeId, EmployeeName, Department,
+                            convert(varchar(10), DateOfJoining,120) as DateOfJoining,
+                            PhotoFileName
+                            from dbo.Employee
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Employee employee)
+        {
+            string query = @"insert into dbo.Employee 
+                            (EmployeeName,Department,DateOfJoining,PhotoFileName)
+                            values 
+                            (
+                            '"+employee.EmployeeName+@"'
+                            ,'"+employee.Department+@"'
+                            ,'"+employee.DateOfJoining+@"'
+                            ,'"+employee.PhotoFileName+@"'
+                            )
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Succesfuly");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Employee employee)
+        {
+            string query = @"
+                            update dbo.Employee set 
+                            EmployeeName = '" + employee.EmployeeName + @"'
+                            ,Department = '" + employee.Department + @"'
+                            ,DateOfJoining = '" + employee.DateOfJoining + @"'
+                            where EmployeeId = "+employee.EmployeeId + @"
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Succesfuly");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Employee
+                            where EmployeeId = " + id + @"
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Delete Succesfuly");
+        }
     }
 }
